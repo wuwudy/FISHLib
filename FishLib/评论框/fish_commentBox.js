3c31d57af6c620bd8fe5a55d463b568e6b559846
wuwudy <2806067876@qq.com>
2024-04-08 23:17:26 +0800

    tmp1

--- FishLib/评论框/fish_commentBox.js
+++ FishLib/评论框/fish_commentBox.js
@@ -4,10 +4,16 @@
   const inputTextarea = document.querySelector('.commentInput')
   const textMountShow = document.querySelector('.commentTextCal')
   const inputTextareaBorder = document.querySelector('.commentTopR')
+  const computedStyle = window.getComputedStyle(inputTextarea)
+  const initialMaxHeight = computedStyle.getPropertyValue('max-height')
   console.log(1)
   inputTextarea.addEventListener('input', (e) => {
-    let textMount = e.target.innerHTML.length
+    let textMount = e.target.value.length
 
+    //输入文字，可能需要增加高度
+    //测试
+    console.log(initialMaxHeight)
+    //删除文字，可能需要减小高度
     textMountShow.innerHTML = `${textMount}/100`
   })
 })()
