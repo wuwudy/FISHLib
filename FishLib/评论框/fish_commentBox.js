210e198cd52caec18bc9da92daf00e1d1e1dc017
wuwudy <2806067876@qq.com>
2024-04-09 15:17:05 +0800

    div模拟textarea实现自适应完成，max-height实现

--- FishLib/评论框/fish_commentBox.js
+++ FishLib/评论框/fish_commentBox.js
@@ -4,8 +4,19 @@
   const inputTextarea = document.querySelector('.commentInput')
   const textMountShow = document.querySelector('.commentTextCal')
   const inputTextareaBorder = document.querySelector('.commentTopR')
+  const computedStyle = getComputedStyle(inputTextarea)
+  const maxHeight = parseInt(computedStyle.getPropertyValue('max-height'))
   console.log(1)
   inputTextarea.addEventListener('input', (e) => {
+    //若没超过最大高度，不显示scroll
+    let nowHeight = parseInt(computedStyle.getPropertyValue('height'))
+    console.log('nowHeight:' + nowHeight + ';' + 'maxHeight:' + maxHeight)
+    if (nowHeight < maxHeight) {
+      e.target.style.overflowY = ''
+    } else {
+      console.log('大了')
+      e.target.style.overflowY = 'scroll'
+    }
     let textMount = e.target.innerHTML.length
     textMountShow.innerHTML = `${textMount}/100`
   })
